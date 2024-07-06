198e10b9325accb6a8e659b20e3679e5c4293d05
Jesufemi-A <jesufemia1@gmail.com>
2024-07-06 04:50:05 -0700

    added User registration Dto for user registration

--- src/main/java/com/HNG_UserAuth/responseDto/UserRegistrationDto.java
+++ src/main/java/com/HNG_UserAuth/responseDto/UserRegistrationDto.java
@@ -1,2 +1,32 @@
-package com.HNG_UserAuth.responseDto;public class UserRegistrationDto {
+package com.HNG_UserAuth.responseDto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+
+    @NotNull
+    @Size(min = 1, message = "First name must not be empty")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1, message = "LastName must not be empty")
+    private String lastName;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    @Size(min = 6, message = "Password must have at least 6 characters")
+    private String password;
+
+
+    @NotNull
+    private String phone;
 }
