232118f5130245f4f6fb14c40994cbc73265e694
Jesufemi-A <jesufemia1@gmail.com>
2024-07-06 04:09:42 -0700

    added User repository

--- src/main/java/com/HNG_UserAuth/repository/UserRepository.java
+++ src/main/java/com/HNG_UserAuth/repository/UserRepository.java
@@ -1,2 +1,14 @@
-package com.HNG_UserAuth.repository;public class UserRepository {
+package com.HNG_UserAuth.repository;
+
+//import com.HNG_UserAuth.models.User;
+import com.HNG_UserAuth.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, String> {
+
+    Optional<String> findUserByUserId(String id);
 }
